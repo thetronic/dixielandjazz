31e481a5259088d4ad17e7c3acb6d90788ea530e
thetronic <stevetronic@gmail.com>
2023-12-20 09:09:23 +0000

    No data fix

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// Variables for accessing Google Sheets API
 var spreadsheetID = '1ejkbveN8ByG3Bk7lKAmiQ4rTMQ922QjbzhJ8g0cWhfY'
 var tabName = 'Sheet1'
 var apiKey = 'AIzaSyBny4imxdqvCz-D4RwDy_Ruk2ENPUK_2CQ'
+
+// Construct the URL for fetching sheet data
 var url =
   'https://sheets.googleapis.com/v4/spreadsheets/' +
   spreadsheetID +
@@ -9,66 +12,83 @@ var url =
   '?alt=json&key=' +
   apiKey
 
+// Initiate a asynchronous GET request to the specified URL
 const request = new XMLHttpRequest()
 request.open('GET', url, true)
 request.send()
-let requestCounter = 10
+let requestCounter = 10 // Counter for retries
 
 request.onload = function () {
-  // REINSTATE FOR FINAL PROJECT
-  console.log('HELLO')
   if (this.readyState === 4 && this.status === 200) {
+    // Request successful, parse JSON response and handle data
     var googleSheetsJSON = JSON.parse(this.responseText)
+    console.log(this.responseText)
     handleLoad(googleSheetsJSON)
   } else {
+    // Request failed, retry up to 10 times
     requestCounter = requestCounter - 1
-    console.log('Failed To Connect: ' + requestCounter)
+
     if (requestCounter >= 0) {
       request.open('GET', url, true)
       request.send()
     } else {
-      console.log('Connection Failed')
+      console.error('Connection Failed')
+      handleLoad(null)
     }
   }
 }
 
 function handleLoad(json) {
-  json.values.shift()
-  console.log(json)
-  updatePage(json.values)
+  if (json) {
+    json.values.shift()
+    updatePage(json.values)
+  } else {
+    updatePage(null)
+  }
 }
 
 function updatePage(jsonEvents) {
   // GENERATE EVENT SCHEMA FOR GOOGLE:
-  const eventScheduledScript = document.createElement('script')
-  eventScheduledScript.setAttribute('type', 'application/ld+json')
+  if (jsonEvents) {
+    const eventScheduledScript = document.createElement('script')
+    eventScheduledScript.setAttribute('type', 'application/ld+json')
 
-  const allEvents = []
-  const latestEvents = document.getElementById('events')
-  latestEvents.textContent = ''
+    const allEvents = []
+    const latestEvents = document.getElementById('events')
+    latestEvents.textContent = ''
 
-  jsonEvents.forEach((event) => {
-    const EventObject = convertToEventObject(event)
-    allEvents.push(EventObject)
-  })
-  console.log('Event Object: ', allEvents)
+    jsonEvents.forEach((event) => {
+      const EventObject = convertToEventObject(event)
+      allEvents.push(EventObject)
+    })
 
-  const limitedEvents = getFirstPastToday(allEvents, 2)
+    const limitedEvents = getFirstPastToday(allEvents, 2)
 
-  console.log('Limited Events: ', limitedEvents)
+    var allEventSchema = []
 
-  var allEventSchema = []
+    limitedEvents.forEach((event) => {
+      const eventCard = createEventCard(event)
+      latestEvents.appendChild(eventCard)
 
-  limitedEvents.forEach((event) => {
-    const eventCard = createEventCard(event)
+      const eventSchema = createEventSchema(event)
+      allEventSchema.push(eventSchema)
+    })
+
+    eventScheduledScript.innerHTML = JSON.stringify(allEventSchema)
+    document.head.appendChild(eventScheduledScript)
+  } else {
+    const latestEvents = document.getElementById('events')
+    const noEvent = {
+      name: '',
+      date: '',
+      time: '',
+      description: '',
+      performer: 'NO UPCOMING SHOWS',
+    }
+    const eventCard = createEventCard(noEvent)
+    latestEvents.textContent = ''
     latestEvents.appendChild(eventCard)
-
-    const eventSchema = createEventSchema(event)
-    allEventSchema.push(eventSchema)
-  })
-
-  eventScheduledScript.innerHTML = JSON.stringify(allEventSchema)
-  document.head.appendChild(eventScheduledScript)
+  }
 }
 
 function convertToEventObject(event) {
@@ -93,14 +113,20 @@ function createEventCard(event) {
   eventDate.className = 'eventDate'
   eventTime.className = 'eventTime'
 
-  eventName.innerHTML = '<b>' + event['performer'].toUpperCase() + '</b>'
-  eventCard.appendChild(eventName)
+  if (event['performer']) {
+    eventName.innerHTML = '<b>' + event['performer'].toUpperCase() + '</b>'
+    eventCard.appendChild(eventName)
+  }
 
-  eventDate.innerHTML = formatDate(event['date'])
-  eventCard.appendChild(eventDate)
+  if (event['date']) {
+    eventDate.innerHTML = formatDate(event['date'])
+    eventCard.appendChild(eventDate)
+  }
 
-  eventTime.innerHTML = event['time']
-  eventCard.appendChild(eventTime)
+  if (event['time']) {
+    eventTime.innerHTML = event['time']
+    eventCard.appendChild(eventTime)
+  }
 
   return eventCard
 }
@@ -193,14 +219,9 @@ function convertDateTime(time, date) {
   var dateParts = date.split('/')
   var timePart = time
 
-  console.log('dateParts', dateParts)
-  console.log('time', time)
-
   // Construct the new date string
   var newDate = new Date(dateParts[2], dateParts[1] - 1, dateParts[0])
-  console.log('newDate', newDate)
   var newDateString = newDate.toISOString().split('T')[0]
-  console.log('newDateString', newDateString)
 
   // Return the new date and time string
   return newDateString + 'T' + timePart
@@ -211,18 +232,12 @@ function getFirstPastToday(arr, limit) {
   let today = new Date()
   today.setHours(0, 0, 0, 0)
 
-  console.log(today)
-
   // Filter the array for dates past today
   let filteredArr = arr.filter((item) => {
     const dateString = item['date']
-    console.log(dateString)
     var dateParts = dateString.split('/')
     // month is 0-based, that's why we need dataParts[1] - 1
     var itemDate = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0])
-    // console.log(dateObject)
-    // let itemDate = new Date(item['date'])
-    console.log(itemDate)
     return itemDate > today
   })
 
